decorr_res validated, normalization of x validated, pareto selection corrected. remains somme commenting and cleanup to do...

src/decorr_res.py
@@ -2,6 +2,8 @@ import skimage.io as skio
 import numpy as np
 import pandas as pd
 
+from inspect import currentframe, getframeinfo
+
 
 def decorr_res(imname=None, image=None):
     #Input image and analysis parameter
@@ -14,7 +16,8 @@ def decorr_res(imname=None, image=None):
 
     # Edge apodization
     edge_apod = (np.sin(np.linspace(-np.pi/2, np.pi/2, w))+1)/2
-    Wx, Wy = np.meshgrid(*(np.concatenate([edge_apod, np.ones(l-2*w), np.flip(edge_apod)]) for l in image.shape))
+#    Wx, Wy = np.meshgrid(*(np.concatenate([edge_apod, np.ones(l-2*w), np.flip(edge_apod)]) for l in image.shape)) #BEFORE
+    Wx, Wy = np.meshgrid(*(np.concatenate([edge_apod, np.ones(l-2*w), np.flip(edge_apod)]) for l in np.flip(image.shape))) #AFTER
     W = Wx*Wy
     mean = image.mean()
     image = W*(image-mean) + mean
@@ -24,7 +27,8 @@ def decorr_res(imname=None, image=None):
     image = image.astype('float32')
     image = image[:image.shape[0]-1+image.shape[0]%2,:image.shape[1]-1+image.shape[1]%2] #Odd array sizes
 
-    X, Y = np.meshgrid(*(np.linspace(-1, 1, l) for l in image.shape)) #J'ai un ordre différent de dans le code matlab
+#    X, Y = np.meshgrid(*(np.linspace(-1, 1, l) for l in image.shape)) #J'ai un ordre différent de dans le code matlab #BEFORE
+    X, Y = np.meshgrid(*(np.linspace(-1, 1, l) for l in np.flip(image.shape))) #AFTER
     R = np.sqrt(X**2 + Y**2)
 
 
@@ -100,18 +104,20 @@ def decorr_res(imname=None, image=None):
     idx_0, A0 = decorr_peak(d, find_max_tol)
     r0 = r[idx_0]
 
-
+    
 
     sigmas = np.exp(np.arange(Ng+1)/Ng*(np.log(2/r0)-np.log(0.15)) + np.log(0.15)) #Not like in the code, Ng+1 values???
     gMax = 2/r0
 
     if gMax==np.inf: gMax = max(image.shape[0],image.shape[1])/2
     sigmas = np.array([image.shape[0]/4] + list(np.exp(np.linspace(np.log(gMax),np.log(0.15),Ng))))
-
+    
 
     idxs = np.array([])
-    As = np.array([])
-    rs = np.array([])
+#    As = np.array([A0]) #BEFORE
+#    rs = np.array([r0]) #BEFORE
+    As = np.array([A0]) #AFTER
+    rs = np.array([r0]) #AFTER
 
     for i, sig in enumerate(sigmas):
         d = decorr(Ik, Ikn, R, r, highpass_sigma=sig)
@@ -119,24 +125,30 @@ def decorr_res(imname=None, image=None):
         idxs = np.append(idxs, idx)
         As = np.append(As, A)
         rs = np.append(rs, r[idx])
-
-        
+    
+#    print("line =", getframeinfo(currentframe()).lineno, "rs=", rs)
     max_freq_peak = rs.max()
     max_freq_peak_idx = np.where(rs == max_freq_peak)[0][-1]
-    if r0>max_freq_peak:
+#    if r0>max_freq_peak: #BEFORE
+    if max_freq_peak_idx==0: #AFTER
         ind1 = 0
-    elif max_freq_peak_idx==(len(sigmas)-1):
-        ind1 = max_freq_peak_idx-1
+    elif max_freq_peak_idx>=(len(sigmas)-1): #AFTER: == changed to >=
+        ind1 = max_freq_peak_idx-2 #AFTER: -1 changed to -2
     else:
-        ind1 = max_freq_peak_idx
+        ind1 = max_freq_peak_idx-1
     ind2 = ind1+1
-
+    
+    
     r1 = rs[max_freq_peak_idx] - (r[1]-r[0])
     r2 = rs[max_freq_peak_idx] + 0.4
     r_finetune = np.linspace(r1, min(r2,r[-1]), Nr)
 
-
+#    import pdb; pdb.set_trace()
+#    print("line =", getframeinfo(currentframe()).lineno, "r_finetune=", r_finetune)
     sigmas_fine_tune = np.exp(np.linspace(np.log(sigmas[ind1]), np.log(sigmas[ind2]) ,Ng))
+    
+    As=As[:-1] #AFTER (weird...)
+    rs=rs[:-1] #AFTER (weird...)
     for i, sig in enumerate(sigmas_fine_tune):
         d = decorr(Ik, Ikn, R, r_finetune, highpass_sigma=sig)
         idx, A = decorr_peak(d, find_max_tol) #Note that in the matlab code, the first element in the array is SNR0, so the length of the array A0 is 12 instead of 11 here
@@ -144,12 +156,15 @@ def decorr_res(imname=None, image=None):
         As = np.append(As, A)
         rs = np.append(rs, r_finetune[idx])
 
-
+#    print("line =", getframeinfo(currentframe()).lineno, "rs=", rs)
+    rs = np.append(rs, r0) #AFTER (nothing before)
+    As = np.append(As, A0) #AFTER (nothing before)
+    
     rs[As<0.05] = 0 # IN the matlab code, Ks set to zero too
     
 
 
-
+    
     res = 2/np.max(rs)
     
     return res

src/user.py
@@ -231,70 +231,104 @@ def select(thetas, objectives, with_time, times, figsize=(10, 10), borders=None)
     # set to your favorite colormap (see https://matplotlib.org/users/colormaps.html)
 #    cmap = pyplot.cm.get_cmap("nipy_spectral")
     cmap = pyplot.cm.get_cmap("cividis")
-
-    title = ax.set_title(f"Pick the best option by clicking on the point. Tmin={numpy.min(times)}, Tmax={numpy.max(times)}")
+    
+    if with_time:
+        title = ax.set_title(f"Pick the best option by clicking on the point. Tmin={numpy.min(times)}, Tmax={numpy.max(times)}")
+    else:
+        title = ax.set_title(f"Pick the best option by clicking on the point.")
 
     # 3 points tolerance
     if with_time:
-#        print(np.dstack(thetas)).squeeze()
-#        print(times)
         time_range = times.max() - times.min() + 1e-11
-        sc = ax.scatter(thetas[0], thetas[1], s=(times-times.min())/time_range * 60 + 20, c=thetas[2], marker="o", alpha=0.5, picker=3, cmap=cmap)
+        
+    if len(thetas)==3:
+        if with_time:
+            sc = ax.scatter(thetas[0], thetas[1], s=(times-times.min())/time_range * 60 + 20, c=thetas[2], marker="o", alpha=0.5, picker=3, cmap=cmap)
+        else:
+            sc = ax.scatter(thetas[0], thetas[1], c=thetas[2], marker="o", alpha=0.5, picker=3, cmap=cmap)
         pyplot.colorbar(sc, ax=ax)
-        if borders is not None:
-            ax.set_xlim(borders[0])
-            ax.set_ylim(borders[1])
-
+    elif len(thetas)==2:
+        if with_time:
+            sc = ax.scatter(thetas[0], thetas[1], s=(times-times.min())/time_range * 60 + 20, marker="o", alpha=0.5, picker=3)
+        else:
+            sc = ax.scatter(thetas[0], thetas[1], marker="o", alpha=0.5, picker=3)
+    if borders is not None:
+        xmin, xmax = borders[0]
+        delta = xmax-xmin
+        ax.set_xlim(xmin-0.1*delta, xmax+0.1*delta)
+        xmin, xmax = borders[1]
+        delta = xmax-xmin
+        ax.set_ylim(xmin-0.1*delta, xmax+0.1*delta)
+        sc.set_clim(borders[2])
+    else:
+#            pyplot.sca(ax)
+#            pyplot.axis('tight')
+        xmin, xmax = numpy.min(thetas[0]), numpy.max(thetas[0])
+        print(xmin, xmax)
+        dx = xmax-xmin
+        if xmin!=xmax:
+            ax.set_xlim(xmin-0.1*dx, xmax+0.1*dx)
+        ymin, ymax = numpy.min(thetas[1]), numpy.max(thetas[1])
+        print(ymin, ymax)
+        dy = ymax-ymin
+        if ymin!=ymax:
+            ax.set_ylim(ymin-0.1*dy, ymax+0.1*dy)
+            
+            
+        
+            
 
 #        lgnd = pyplot.legend(loc="lower left", numpoints=1, fontsize=100)
-    elif len(objectives) > 2:
-        sc = ax.scatter(thetas[0], thetas[1], s=100, c=times, marker="o", alpha=0.5, picker=3, cmap=cmap)
-        pyplot.colorbar(sc, ax=ax)
-        
-        
-    else:
-        ax.scatter(thetas[0], thetas[1], s=200, marker="o", alpha=0.5, picker=3)
-    ax.grid(True)
-    ax.set_xlabel(objectives[0].label)
-    ax.set_ylabel(objectives[1].label)
+#    elif len(objectives) > 2:
+#        sc = ax.scatter(thetas[0], thetas[1], s=100, c=times, marker="o", alpha=0.5, picker=3, cmap=cmap)
+#        pyplot.colorbar(sc, ax=ax)
+#
+#
+#    else:
+#        ax.scatter(thetas[0], thetas[1], s=200, marker="o", alpha=0.5, picker=3)
+#    ax.grid(True)
+#    ax.set_xlabel(objectives[0].label)
+#    ax.set_ylabel(objectives[1].label)
 
-    # handle adding custom ticks on mirror axis
-    new_ticks_x = objectives[0].mirror_ticks(ax.get_xticks())
-    if new_ticks_x is not None:
-        ax2 = ax.twiny()
-        ax2.set_xlim(ax.get_xlim())
-        ax2.set_xticklabels(new_ticks_x)
-        title.set_y(1.05)
-
-        if with_time:
-            sc.remove()
-            sc = ax2.scatter(thetas[0], thetas[1], s=100, c=times, marker="o", alpha=0.5, picker=3, cmap=cmap)
-        elif len(objectives) > 2:
-            sc.remove()
-            sc = ax2.scatter(thetas[0], thetas[1], s=100, c=thetas[2], marker="o", alpha=0.5, picker=3, cmap=cmap)
-            print("--------------------------------------awrefsdrgsdrfgsdf dsf---------1")
-            
-        else:
-            sc.remove()
-            sc = ax2.scatter(thetas[0], thetas[1], s=200, marker="o", alpha=0.5, picker=3)
-
-    new_ticks_y = objectives[1].mirror_ticks(ax.get_yticks())
-    if new_ticks_y is not None:
-        ax3 = ax.twinx()
-        ax3.set_ylim(ax.get_ylim())
-        ax3.set_yticklabels(new_ticks_y)
-
-        if with_time:
-            sc.remove()
-            sc = ax3.scatter(thetas[0], thetas[1], s=100, c=times, marker="o", alpha=0.5, picker=3,
-                             cmap=cmap)
-        elif len(objectives) > 2:
-            sc.remove()
-            sc = ax3.scatter(thetas[0], thetas[1], s=100, c=thetas[2], marker="o", alpha=0.5, picker=3,
-                             cmap=cmap)
-        else:
-            sc.remove()
-            sc = ax3.scatter(thetas[0], thetas[1], s=200, marker="o", alpha=0.5, picker=3)
+#    # handle adding custom ticks on mirror axis
+#    new_ticks_x = objectives[0].mirror_ticks(ax.get_xticks())
+#    if new_ticks_x is not None:
+#        ax2 = ax.twiny()
+#        ax2.set_xlim(ax.get_xlim())
+#        ax2.set_xticklabels(new_ticks_x)
+#        title.set_y(1.05)
+#
+#        if with_time:
+#            import pdb; pdb.set_trace()
+#            sc.remove()
+#            sc = ax2.scatter(thetas[0], thetas[1], s=100, c=times, marker="o", alpha=0.5, picker=3, cmap=cmap)
+#        elif len(objectives) > 2:
+#            sc.remove()
+#            sc = ax2.scatter(thetas[0], thetas[1], s=100, c=thetas[2], marker="o", alpha=0.5, picker=3, cmap=cmap)
+#            print("--------------------------------------awrefsdrgsdrfgsdf dsf---------1")
+#
+#        else:
+#            sc.remove()
+#            sc = ax2.scatter(thetas[0], thetas[1], s=200, marker="o", alpha=0.5, picker=3)
+#
+#    new_ticks_y = objectives[1].mirror_ticks(ax.get_yticks())
+#    if new_ticks_y is not None:
+#        ax3 = ax.twinx()
+#        ax3.set_ylim(ax.get_ylim())
+#        ax3.set_yticklabels(new_ticks_y)
+#
+#        if with_time:
+#            import pdb; pdb.set_trace()
+#            sc.remove()
+#            sc = ax3.scatter(thetas[0], thetas[1], s=100, c=times, marker="o", alpha=0.5, picker=3,
+#                             cmap=cmap)
+#        elif len(objectives) > 2:
+#            sc.remove()
+#            sc = ax3.scatter(thetas[0], thetas[1], s=100, c=thetas[2], marker="o", alpha=0.5, picker=3,
+#                             cmap=cmap)
+#        else:
+#            sc.remove()
+#            sc = ax3.scatter(thetas[0], thetas[1], s=200, marker="o", alpha=0.5, picker=3)
 
     def onpick(event):
         """Handles the event from the :mod:`matplotlib` to select the points. It
@@ -304,18 +338,20 @@ def select(thetas, objectives, with_time, times, figsize=(10, 10), borders=None)
         """
         global index
         # handle the situation where several points overlap
-        if with_time:
-            print("Selected points:", event.ind)
-            min_z = numpy.min(times[event.ind])
+#        if with_time:
+#            print("Selected points:", event.ind)
+#            min_z = numpy.min(times[event.ind])
 #            candidates = event.ind[times[event.ind] == min_z]
-            candidates = event.ind
-        elif len(objectives) > 2:
-            print("Selected points:", event.ind)
-            # objectives are minimized (see objectives.py)
-            min_z = numpy.min(thetas[2][event.ind])
-            candidates = event.ind[thetas[2][event.ind] == min_z]
-        else:
-            candidates = event.ind
+#            candidates = event.ind
+#        elif len(objectives) > 2:
+#            print("Selected points:", event.ind)
+#            # objectives are minimized (see objectives.py)
+#            min_z = numpy.min(thetas[2][event.ind])
+#            candidates = event.ind[thetas[2][event.ind] == min_z]
+#        else:
+#            candidates = event.ind
+        candidates = event.ind
+        
         print("Picking at random in", candidates)
         index = numpy.random.choice(candidates)
 