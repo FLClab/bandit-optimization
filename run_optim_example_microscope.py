080fc407322a5ae033d7e39e303a238ca237937c
albertmg1 <albertmganon@me.com>
2021-08-03 16:10:41 -0400

    changed the way to specify the prior, added the nsga option, time threshold

--- run_optim_example_microscope.py
+++ run_optim_example_microscope.py
@@ -1,60 +1,57 @@
 import banditopt.optim_microscope as optim
-import numpy as np
 
-# x_mins=[(4e-3)*2**-2, (1e-5)*2**-2]
-# x_maxs=[(4e-3)*2**2, (1e-5)*2**2]
+x_mins=[5, 5, 10e-6]
+x_maxs=[95, 95, 500e-6]
+
+degree = 3
+
+borders = {"Resolution":(0,350), "Bleach":(0,1), "SNR":(0,40)}
+
 
 config =  dict(
-    save_folder="sted_deg2_autoreg",
+    save_folder=f"2020-07-12-4params/deg{degree}",
     regressor_name="sklearn_BayesRidge",
     regressor_args= {
         "default":{
-            "alpha_1": 1e-6,
-            "alpha_2": 1e-6,
-            "alpha_init": 100.11183053174646,
-            "compute_score": True,
-            "degree": 2,
-            "fit_intercept": True,
-            "lambda_1": 1e-6,
-            "lambda_2": 1e-6,
-            "lambda_init": 1.4595412992911199,
-            "tol": 1.0e-06,
-            'param_space_bounds':None,
+            "degree": degree,
+            'param_space_bounds':[(x_mins[i], x_maxs[i]) for i in range(len(x_mins))],
         },
-        "SNR":{"alpha_init":1./0.5**2, "lambda_init":1/(2*15/(0.028)**2)**2,},
-        "Bleach":{"alpha_init":1./0.06**2, "lambda_init":1/(2/(0.028)**2)**2,},
         "Resolution":{"alpha_init":1./3.**2, "lambda_init":1/(2*100/(0.028)**2)**2,},
+        "Bleach":{"alpha_init":1./0.06**2, "lambda_init":1/(2/(0.028)**2)**2,},
+        "SNR":{"N0_w":1, "std0_w":borders[0], N0_n:1, N0_w:},
     },
-    n_divs_default = 25,
-    param_names = ["p_ex"],
+     n_divs_default = 20, #20**3 = 8000
+#    n_points = [10, 10, 10, 10],
+    param_names = ["p_ex", "p_sted", "dwelltime", "line_step"],
     # param_names = ["p_ex"],
-    with_time=True,
+    with_time=False,
     default_values_dict=dict(
-        molecules_disposition = None,
-        im_size_nm  = 1000,
-        nb_molecules_per_point_src = 50,
-        nb_pt_src = 8,
-        pixelsize = 20e-9,
-        p_ex = 50e-6,
-        p_sted = 3e-6,
-        dwelltime = 10e-6,
-        bleach = True,
-        noise = True,
-        background=5/10e-6,
-        darkcount=0
+        # molecules_disposition = None,
+        # im_size_nm  = 1000,
+        # nb_molecules_per_point_src = 50,
+        # nb_pt_src = 8,
+        pixelsize = 20e-9, # I verified that the real pixel size is 20e-9, also, 10 radom points initially hardcoded
+        # p_ex = 50e-6,
+        # p_sted = 3e-6,
+        # dwelltime = 10e-6,
+        # bleach = True,
+        # noise = True,
+        # background=5/10e-6,
+        # darkcount=0
     ),
-    params_conf = { 'p_ex':20e-6, 'p_sted':0, },
-    x_mins=[0],
-    x_maxs=[95],
-    obj_names=["Resolution", "Bleach", ],
+    params_conf = None,
+    x_mins=x_mins,
+    x_maxs=x_maxs,
+    obj_names=["Resolution", "Bleach", 'SNR'],
     # obj_names=["SNR", "Bleach", ],
-    optim_length = 60,
-    nbre_trials = 5,
-#    borders = [(0,300), (0,1), (0,50)],
-    borders = None,
-    pareto_only = False,
-    split_sted_params = dict(p_ex=5, p_sted=5, nb_images=8, tau_exc=np.inf,),
+    optim_length = 150,
+    nbre_trials = 3,
+    borders = borders,
+    # borders = None,
+    pareto_only = True,
+    time_limit = 500e-6 #total_time/nbre_pixels
+    NSGAII_kwargs = dict(NGEN=250, MU=200, L = 40, min_std=np.sqrt(2e-4*len(obj_names)))
               )
 
-
 optim.run_TS(config=config, **config)
+
